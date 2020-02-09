9ab9d016332e2010d1fd77e8f5103a5a2343f4bb
ivoryjingyiwang <60619189+ivoryjingyiwang@users.noreply.github.com>
2020-02-09 22:31:33 +0000

    explanation

--- serial_LED_control/serial_LED_control.ino
+++ serial_LED_control/serial_LED_control.ino
@@ -6,19 +6,21 @@ pinMode(LED_BUILTIN, OUTPUT);
 
 Serial.begin(9600);
 
-while (!Serial);
+while (!Serial); //wait for serial port to connect, needed for native usb only
 
-Serial.println("Input 1 to Turn LED on and 2 to off");
+Serial.println("Input 1 to Turn LED on and 0 to off");
 
 }
 
 void loop() {
 
-if (Serial.available())
-
+if (Serial.available()) //returns the number of characters (i.e. bytes of data) 
+                       //which have arrived in the serial buffer and that are ready to be read.
+//Serial.read() returns the first (oldest) character in the buffer and removes that byte of data from the buffer.
+//So when all the bytes of data are read and no new serial data have arrived, the buffer is empty and Serial.available() will return 0.
 {
 
-int state = Serial.parseInt();
+int state = Serial.parseInt();//Looks for the next valid integer in the incoming serial. The function terminates if it times out
 
 if (state == 1)
 
--- serial_test.py
+++ serial_test.py
@@ -4,23 +4,25 @@ import serial
 import time
 
 #initialise a serial port (name of port, baud rate). The name will likely be different on your computer
-ser = serial.Serial('/dev/cu.usbmodem14101',9600)
+ser = serial.Serial('/dev/cu.usbmodem14101',9600) # open serial port
+print(ser.name)         # check which port was really used
 
 #read initial serial message
-answer = ser.readline()
-print(answer.decode('utf-8'))
+answer = ser.readline() # read a '\n' terminated line
+print(answer.decode('utf-8')) #transform the unicode to readable code
 
 
 #turn LED off and on in infinite loop
 LED_status = 1
-while True:
-    LED_status = int(not LED_status)
-    command = str(LED_status)
-    print(f'Sent: {command}')
-    ser.write(command.encode('utf-8'))
-    response = ser.readline()
+while True: #infinite loop 
+    LED_status = int(not LED_status) #close led
+    command = str(LED_status) #string of status
+    print(f'Sent: {command}') #f-strings: formatted string literal, and write 
+                              #expressions in {}
+    ser.write(command.encode('utf-8'))#write the command above after change it into utf-8
+    response = ser.readline() #read in the status of port
     print(f'Response: {response.decode("utf-8")}')
-    time.sleep(1)
+    time.sleep(1) #Suspend execution of the loop for the given number of seconds.
 
 
 
